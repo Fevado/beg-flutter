44314ef6469758a7f6057f4f00dcfa7f14ba8ee8
favoured mwange <mwangefavoured@gmail.com>
2025-08-06 23:25:01 +0300

    Learnt how to make dart variables

---
+++ variable.dart
@@ -0,0 +1,60 @@
+// variable with no specified type
+var name = 'vad';
+
+// variable declared with type
+String filter = 'company';
+
+// uninitialize variable initial value is null
+// String friend;
+
+// variable value will not change
+final man = 'male';
+final String home = 'Kenya';
+const goat = 'messi';
+const String ronaldo = 'goat';
+
+// public variable
+String username = 'Auntyy';
+
+// private variable(declared using an underscore)
+String _role = 'Admin';
+
+// declaring numbers
+int age = 23; //for whole numbers
+double price = 0.0; // for decimal numbers
+
+// Strings
+String defaultMenu = 'main';
+
+// String concatenation
+String firstName = 'Favoured';
+String secondName = 'Mwange';
+String combinedName = 'main' + '' + 'function';
+String combinedNameNoPlusSign =
+    'main'
+    ''
+    'functiond';
+String multilineAddress = '''
+  123 Any Street
+  City, State, Zip
+''';
+
+// Booleans
+bool isDone = false;
+
+main() {
+  print('Hi my name is ${firstName} ${secondName}');
+  print(combinedName);
+  print(combinedNameNoPlusSign);
+  print(multilineAddress);
+}
+
+var jina = 'Vad';
+final id = 7685433;
+final int idd = 7685433;
+const iD = 7685433;
+const int iDD = 7685433;
+String bestie = 'Auntyy';
+double height = 5.44;
+int weight = 65;
+String _userId = '1234'; // this one is a private variable
