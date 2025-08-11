7f33b542be090f1450e0aa009d5b6ce8e95382f1
favoured mwange <mwangefavoured@gmail.com>
2025-08-11 22:58:08 +0300

    Learns about encapsulation oop

---
+++ encapsulation.dart
@@ -0,0 +1,41 @@
+class Circle {
+  // create a private variable _radius
+  double _radius;
+
+  Circle(this._radius);
+
+  // getter method to access the radius
+  double get radius => _radius;
+
+  // settermethod to update radius with validation
+  set radius(double value) {
+    if (value > 0) {
+      _radius = value;
+    } else {
+      print('Radius must be greater than zero');
+    }
+  }
+
+  // method to calculate area of circle
+  double calcArea() {
+    return 3.142 * _radius * _radius;
+  }
+
+  double calcPerim() {
+    return 2 * 3.142 * _radius;
+  }
+}
+
+void main() {
+  Circle circle = Circle(7);
+  print('Initial radius ${circle.radius}');
+  print("Initial area ${circle.calcArea()}");
+  print("Initial perimeter ${circle.calcPerim()}");
+
+  // updating the radius of circle
+  circle.radius = 14.0;
+print('updated radius ${circle.radius}');
+print("updated area ${circle.calcArea()}");
+print("updated perimeter ${circle.calcPerim()}");
+
+}
