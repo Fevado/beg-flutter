faa602af459844c895b24023d08911414c6767fb
favoured mwange <mwangefavoured@gmail.com>
2025-08-19 10:59:01 +0300

    Completes async programming exercise 3

---
+++ asyncProgramming_assign3.dart
@@ -0,0 +1,18 @@
+Future<String> fetchPosts() async {
+  await Future.delayed(Duration(seconds: 3));
+
+  return 'Posts fetched';
+}
+
+Future<String> fetchComments() async {
+  await Future.delayed(Duration(seconds: 2));
+
+  return 'Comments fetched';
+}
+
+Future<void> main() async {
+  print('Retrieving content');
+
+  var result = await Future.wait([fetchPosts(),fetchComments()]);
+  print(result);
+}
