3717df4e0a621bb242c831d75a7e6b3dcba1d006
favoured mwange <mwangefavoured@gmail.com>
2025-08-26 14:36:58 +0300

    Practice deep into the flutter widget tree

--- ch4_starter_exercise/lib/pages/about.dart
+++ ch4_starter_exercise/lib/pages/about.dart
@@ -9,10 +9,122 @@ class _AboutState extends State<About> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('About')
-        ),
-      body: Container(),
+      appBar: AppBar(title: Text('About Page')),
+      body: SafeArea(
+        child: SingleChildScrollView(
+         child: Padding(
+          padding: EdgeInsets.all(16.0),
+          child: Column(
+            children: <Widget>[
+              Row(
+                children: <Widget>[
+                  Container(
+                    color: Colors.yellow,
+                    height: 40.0,
+                    width: 40.0,
+                  ),
+                  Padding(padding: EdgeInsets.all(16.0),),
+                  Expanded(
+                    child: Container(
+                      color: Colors.amber,
+                      height: 40.0,
+                      width: 40.0,
+                    ),
+                    ),
+                    Padding(padding: EdgeInsets.all(16.0),),
+                    Container(
+                      color: Colors.brown,
+                      height: 40.0,
+                      width: 40.0,
+                    )
+                ],
+              ),
+              Padding(padding: EdgeInsets.all(16.0),),
+              Row(
+                children: <Widget>[
+                  Container(
+                    color: Colors.yellow,
+                    height: 40.0,
+                    width: 40.0,
+                  ),
+                  Padding(padding: EdgeInsets.all(16.0),),
+                  Expanded(
+                    child: Container(
+                      color: Colors.amber,
+                      height: 40.0,
+                      width: 40.0,
+                    ),
+                    ),
+                    Padding(padding: EdgeInsets.all(16.0),),
+                    Container(
+                      color: Colors.brown,
+                      height: 40.0,
+                      width: 40.0,
+                    )
+                ],
+              ),
+              Padding(padding: EdgeInsets.all(16.0),),
+              Row(
+                children: <Widget>[
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisSize: MainAxisSize.max,
+                    children: <Widget>[
+                      Container(
+                        color: Colors.yellow,
+                        height: 60.0,
+                        width: 60.0,
+                      ),
+                      Padding(padding: EdgeInsets.all(16.0),),
+                      Container(
+                        color: Colors.amber,
+                        height: 40.0,
+                        width: 40.0,
+                      ),
+                      Padding(padding: EdgeInsets.all(16.0),),
+                      Container(
+                        color: Colors.brown,
+                        height: 20.0,
+                        width: 20.0,
+                      ),
+                      Divider(),
+                      Row(
+                        children: <Widget>[
+                          CircleAvatar(
+                            backgroundColor: Colors.lightGreen,
+                            radius: 100,
+                            child: Stack(
+                              children: <Widget>[
+                                Container(
+                                  height: 100.0,
+                                  width: 100.0,
+                                  color: Colors.yellow,
+                                ),
+                                Container(
+                                  height: 60,
+                                  width: 60,
+                                  color: Colors.amber,
+                                ),
+                                Container(
+                                  height: 40.0,
+                                  width: 40.0,
+                                  color: Colors.brown,
+                                )
+                              ],
+                            ),
+                          )
+                        ],
+                      ),
+                      Divider(),
+                      Text('End of the line'),
+                    ],
+                  )
+                ],
+              )
+            ],
+          ),
+          ),
+      )),
     );
   }
 }
